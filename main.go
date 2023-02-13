e0f2cf3363965ad666c5ccb53b3b3a8b8f99d18b
q56535 <963750519@qq.com>
2023-02-13 11:58:30 +0800

    添加内容

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
 
 func main() {
-	fmt.Println("aa")
+	h := server.Default(server.WithHostPorts("127.0.0.1:8080"))
+
+	h.GET("/hello", func(ctx context.Context, c *app.RequestContext) {
+		c.String(consts.StatusOK, "OK")
+	})
+
+	h.Spin()
 }
